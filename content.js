f61d0aec68253940546f3115ea91dc5dde024760
Chris DeVisser <chris.n.devisser@gmail.com>
2018-06-16 17:30:36 -0400

    Rewrite to be mostly webNavigation-based
    With webNavigation events, it's possible to detect when the user navigates to the new tab page and what type of navigation it is (specifically, forward_back).
    
    Because Chrome doesn't provide button info in mouseup events, the content script keeps track of whether there was just a back button down event before it takes action on a button up event. This prevents things like closing a tab on button down and the remaining tab getting the button up.
    
    The content script uses the history size to determine whether to close the tab or let the back press go through and trigger navigation events.

--- content.js
+++ content.js
@@ -1,9 +1,22 @@
-document.addEventListener('keyup', e => {
-    const INSERT = 45;
-    if (e.keyCode !== INSERT) return;
-    chrome.runtime.sendMessage({
-        msg: 'closeTab',
-        referrer: document.referrer,
-        historyLength: history.length
-    });
+let g_backButtonDownOccurred = false;
+
+document.addEventListener('mousedown', e => {
+    g_backButtonDownOccurred = false;
+
+    const BACK_BUTTON = 8;
+    if (e.buttons !== BACK_BUTTON) return;
+
+    g_backButtonDownOccurred = true;
+});
+
+document.addEventListener('mouseup', e => {
+    if (!g_backButtonDownOccurred) return;
+
+    if (history.length === 1) {
+        console.log('Making back click close tab')
+        e.preventDefault();
+        chrome.runtime.sendMessage({
+            msg: 'closeTab'
+        });
+    }
 });
